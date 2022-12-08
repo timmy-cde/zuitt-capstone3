7749b305219f8c78354828a83960e259f2fb85c4
Timothy Manuel <timothys.manuel@gmail.com>
2022-12-08 12:48:34 +0800

    updated files

--- backend/index.js
+++ backend/index.js
@@ -24,24 +24,24 @@ db.once("open", () => console.log("We're connected to the MongoDb database"));
 // Middleware
 // Allow all resources to access our backend application
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '2.1mb' }));
+app.use(express.urlencoded({ limit: '2.1mb', extended: true }));
 
 // Routes for our API
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 
-app.get("/", (_, res) => {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-    res.sendFile(
-        path.resolve(__dirname, 'frontend', 'build', 'index.html'), (err) => {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    )
-})
+// app.get("/", (_, res) => {
+//     app.use(express.static(path.resolve(__dirname, '../frontend', 'build')));
+//     res.sendFile(
+//         path.resolve(__dirname, 'frontend', '../build', 'index.html'), (err) => {
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//         }
+//     )
+// })
 
 // Listening to port
 app.listen(process.env.PORT || port, () => {
